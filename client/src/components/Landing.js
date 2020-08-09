3aaf65df2cc9323ccf1505ec8044742fcc632bc0
Pieter <pieterpretorius223@gmail.com>
2020-08-09 21:47:05 +0200

    initial commit

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return(
+    <div style={{ textAlign: 'center' }}>
+      <h1>Emaily</h1>
+      <hr />
+      <br />
+      <h4>Collect feedback from your users</h4>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+const { session } = require('passport');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express serve productions assets like main.js or main.css
+  app.use(express.static('/client/build'));
+
+  // Express serve up index.html if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server has started");
+});
